3ed5ea440a4595b55d140415d31d6e8b2c0044b2
Orduhani Riza <orduhaniriza@gmail.com>
2025-01-28 20:00:06 +0300

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './header.jsx'
+import Footer from './Footer.jsx'
+
+function App() {
+      return(
+        <>
+        <Header></Header>
+        <Footer></Footer>
+        </>
+      )
+}
+
+export default App
---
+++ my-react-app/src/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <h1>My first React application</h1>
+            <ul>
+                <li>Home</li>
+                <li>Careers</li>
+                <li>About</li>
+                <li>Contacts</li>
+            </ul>   
+        </header>
+    )
+}
+
+export default Header
