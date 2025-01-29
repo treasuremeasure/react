c6c7fc9e8c067f4122cacf2e1afbe79348ccf181
Riza <orduhaniriza@gmail.com>
2025-01-29 21:15:31 +0300

    updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,11 @@
 
-import Card from './Card.jsx'
+import Student from './Student.jsx'
 function App() {
     return (
-      <Card/>
+      <>
+      <Student name="Spongebob" age={30}  isStudent = {true}/>
+      <Student name="Patrick" age={45}  isStudent = {true}/>
+      </>
     )
 }
 
