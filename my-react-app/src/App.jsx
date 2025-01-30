ff52e4103d2bdeec4d56db57649a7320699a03fb
Riza <orduhaniriza@gmail.com>
2025-01-30 21:05:05 +0300

    updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,10 @@
 
-import UserGreeting from './UserGreeting.jsx'
+import List from './List.jsx'
+
 function App() {
     return (
       <>
-      <UserGreeting isLoggedIn = {true} username = "Riza"/>
+      <List/>
       </>
     )
 }
