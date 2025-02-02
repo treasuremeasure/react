c719709852c202fdae0648dfa8285601ac86333c
Orduhani Riza <orduhaniriza@gmail.com>
2025-02-02 16:58:24 +0300

    updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
 
-import MyComponent from './MyComponent.jsx'
+import Counter from './MyComponent.jsx'
 
 function App() {
     return (
       <>
-      <MyComponent/>
+      <Counter/>
       </>
     )
 }
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,18 +1,29 @@
 import React, {useState} from 'react'
 
-function MyComponent() {
-    const [name, setName] = useState()
+function Counter() {
+    const [count, setCount] = useState(0)
 
-    function handleNameChange() {
-        setName(event.target.value)
+    const IncreaseCount = () => {
+        setCount(count + 1)
+        setCount(count + 1)
+    }
+ 
+    const DecreaseCount = () => {
+        setCount(count - 1)
     }
 
+    const Reset = () => {
+        setCount(0)
+    }
+    
     return (
         <div>
-            <input value={name} onChange={handleNameChange}/>
-            <p>Name: {name}</p> 
+            Count: {count}<br></br> <br></br>
+            <button onClick={IncreaseCount}>Increase</button><br></br> <br></br>
+            <button onClick={DecreaseCount}>Decrease</button>
+            <button onClick={Reset}>Reset</button>
         </div>
     )
 }
 
-export default MyComponent
+export default Counter
