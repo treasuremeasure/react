7698c493376fb904b1335464481f367979e23833
Orduhani Riza <orduhaniriza@gmail.com>
2025-02-12 20:59:34 +0300

    updates

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,11 +2,11 @@ import React, {useState, useEffect} from "react"
 
 function MyComponent() {
 
-    const [count, setCount] = useState()
+    const [count, setCount] = useState(0)
 
     useEffect(() => {
         document.title = `Count: ${count}`
-    }, [count])
+    }, [])
 
     return (
         <>
