67741172d96190dd6b64562eb83923e8f41ff204
Riza <orduhaniriza@gmail.com>
2025-02-13 07:43:04 +0300

    updates

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,10 @@
-import MyComponent from './MyComponent.jsx'
+import DigitalClock from './DigitalClock.jsx'
 
 
 function App() {
     return (
       <>
-      <MyComponent/>
+      <DigitalClock/>
       </>
     )
 }
